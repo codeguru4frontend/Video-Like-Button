a6f18849663d5aedddde6f56a02abd0399af6dc8
codeguru4frontend <codeguru4frontend@gmail.com>
2023-12-08 11:51:51 +0100

    added Video-Like-Button

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Write your solution here */
+/* Write your solution here */
+const likeButton = document.querySelector('button');
+likeButton.addEventListener('click', function() {
+console.log("clicked");
+const likeCount = document.querySelector('.like-count');
+likeCount.innerText = "1024";
+});
